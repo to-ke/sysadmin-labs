3113f283aaabb27ee34004e236f03b61c97e2d80
to-ke <goodnoodles2233@gmail.com>
2024-10-05 23:42:03 -0700

    Finished Phonebook

--- 2/phonebook_skeleton.sh
+++ 2/phonebook_skeleton.sh
@@ -7,24 +7,54 @@ if [ "$#" -lt 1 ]; then
     exit 1
 
 elif [ "$1" = "new" ]; then
-    # YOUR CODE HERE #
+    echo "$2, $3" >> "$PHONEBOOK_ENTIRES"
 
 elif [ "$1" = "list" ]; then
     if [ ! -e $PHONEBOOK_ENTRIES ] || [ ! -s $PHONEBOOK_ENTRIES ]; then
-        echo "phonebook is empty"
+    "phonebook is empty"
     else
-        # YOUR CODE HERE #
+        while IFS= read -r line; do
+		echo "$line"
+	done < "$PHONEBOOK_ENTRIES"
     fi
 
 elif [ "$1" = "lookup" ]; then
-    # YOUR CODE HERE #
+    while IFS=read -r line; do
+	    name=$(echo "$line" | awk '{$NF=""; print $0}')
+	    phoneNumber=$(echo "$line" | awk '{print $NF}')
+
+	    if [[ "$name" == *"$2"* ]]; then
+		    echo "$phone_number"
+	    fi
+
+    done < "$PHONEBOOK_ENTRIES"
 
 elif [ "$1" = "remove" ]; then
-    # YOUR CODE HERE #
+
+	temp_file=$(mktemp)
+
+	if [ -e "$PHONEBOOK_ENTRIES" ] && [ -s "$PHONEBOOK_ENTRIES" ]; then
+		while IFS= read -r line; do
+			name=$(echo "$line" | awk '{$NF=""; print $0}' | xargs)
+	    		
+			if [[ "$name" != *"$2"* ]]; then
+		    		echo "$line" >> "$temp_file"
+	    		fi
+		
+		done < "$PHONEBOOK_ENTRIES"
+		
+		mv "$temp_file" "$PHONEBOOK_ENTRIES"
+
+	else
+		rm "$temp_file"
+	fi
+
 
 elif [ "$1" = "clear" ]; then
-    # YOUR CODE HERE #
+    if [ -e "$PHONEBOOK_ENTRIES" ]; then
+	    rm "$PHONEBOOK_ENTRIES"
+fi
 
 else
-     # YOUR CODE HERE #
+     exit 1
 fi
